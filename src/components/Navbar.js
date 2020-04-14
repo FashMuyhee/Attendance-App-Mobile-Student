7c51d58f994d1975662effaaa21339732425c172
fashmuyhee <fashmuyidavid@gmail.com>
2020-04-14 14:16:33 +0100

    changed title alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({title, subTitle, textStyle, leftAction, rightAction}) => {
       title={title}
       titleStyle={textStyle}
       subtitle={subTitle}
-      alignment="center"
+      alignment="start"
       style={styles.navBar}
       rightControls={rightAction}
       leftControl={leftAction}

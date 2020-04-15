f33deb20b7389ee2f0908e5fffa5a02d32e9d5c9
fashmuyhee <fashmuyidavid@gmail.com>
2020-04-15 02:20:22 +0100

    disable yellowbox

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import MainApp from './MainApp';
-
+console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => MainApp);

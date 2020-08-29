01ac2581bbb0c8e607a382294310ff4d62b482f4
Oluwamuyiwa David <fashmuyidavid@gmail.com>
2020-08-29 05:53:45 +0100

    used switched navigator

--- src/route/navigator.js
+++ src/route/navigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs, CardStyleInterpolators, TransitionPresets } from '@react-navigation/stack';
 import {
   HomeScreen,
   SettingsScreen,
@@ -13,28 +13,61 @@ import {
   MyAttendance
 } from '../views';
 
+import { inject, observer } from 'mobx-react';
+
 const Stack = createStackNavigator();
-/* const MainNavigator = () => (
-  <Stack.Navigator headerMode="none">
-
-  </Stack.Navigator>
-); */
-
-const OnboardingNavigator = () => (
-  <Stack.Navigator headerMode="none" initialRouteName="landing">
+/* const OnboardingNavigator = () => (
+  <Stack.Navigator headerMode="none" initialRouteName="landing" screenOptions={{
+    /*  transitionSpec: {
+       open: TransitionSpecs.TransitionIOSSpec,
+       close: TransitionSpecs.TransitionIOSSpec,
+     }, 
+    gestureEnabled: true,
+    ...TransitionPresets.SlideFromRightIOS,
+    // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+  }}>
     <Stack.Screen name="landing" component={LandingScreen} />
     <Stack.Screen name="signup" component={SignUpScreen} />
     <Stack.Screen name="signin" component={SignInScreen} />
-    <Stack.Screen name="home" component={HomeScreen} />
-    <Stack.Screen name="take" component={TakeScreen} />
-    <Stack.Screen name="settings" component={SettingsScreen} />
-    <Stack.Screen name="my_course" component={MyCourse} />
-    <Stack.Screen name="add_course" component={AddCourse} />
-    <Stack.Screen name="my_attendance" component={MyAttendance} />
-  </Stack.Navigator>
+  </Stack.Navigator >
 );
-export const AppNavigator = () => (
+ */
+
+const MainNavigator = ({ store }) => {
+  const { userToken } = store
+  return (
+    <Stack.Navigator headerMode="none" initialRouteName="landing" screenOptions={{
+      /*  transitionSpec: {
+         open: TransitionSpecs.TransitionIOSSpec,
+         close: TransitionSpecs.TransitionIOSSpec,
+       }, */
+      gestureEnabled: true,
+      ...TransitionPresets.SlideFromRightIOS,
+      // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+    }}>
+      {userToken == '' ? (
+        <>
+          <Stack.Screen name="landing" component={LandingScreen} />
+          <Stack.Screen name="signup" component={SignUpScreen} />
+          <Stack.Screen name="signin" component={SignInScreen} />
+        </>) : (
+          <>
+            <Stack.Screen name="home" component={HomeScreen} />
+            <Stack.Screen name="take" component={TakeScreen} />
+            <Stack.Screen name="settings" component={SettingsScreen} />
+            <Stack.Screen name="my_course" component={MyCourse} />
+            <Stack.Screen name="add_course" component={AddCourse} />
+            <Stack.Screen name="my_attendance" component={MyAttendance} />
+          </>)}
+
+    </Stack.Navigator >
+  );
+}
+
+const AppNavigator = () => (
   <NavigationContainer>
-    <OnboardingNavigator />
+    <MainNavigator />
   </NavigationContainer>
 );
+
+export default inject('store')(observer(AppNavigator));

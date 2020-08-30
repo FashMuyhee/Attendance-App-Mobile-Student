28cffe6f1a1f5b1be621c25a7096c978dda89c62
Oluwamuyiwa David <fashmuyidavid@gmail.com>
2020-08-30 20:56:16 +0100

    replace imagepicker wuth rn-camera

--- src/views/step/Camera.js
+++ src/views/step/Camera.js
@@ -1,5 +1,5 @@
-/* eslint-disable react-native/no-inline-styles */
-import React, { useState } from 'react';
+import React, { PureComponent } from 'react';
+import { StyleSheet, View } from 'react-native';
 import {
   StyleService,
   useStyleSheet,
@@ -8,137 +8,119 @@ import {
   Icon,
   useTheme,
 } from '@ui-kitten/components';
-import { Image } from 'react-native';
-import { Container, ModalAlert } from '../../components';
+import { Container, ModalAlert, MyText } from '../../components';
 import ImagePicker from 'react-native-image-picker';
 import { inject, observer } from 'mobx-react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import { View } from 'react-native';
-import axios from 'axios';
-const Camera = (props) => {
-  const styles = useStyleSheet(themedStyles);
-  const theme = useTheme();
-  const [state, setState] = useState({
+// import axios from 'axios';
+import { RNCamera } from 'react-native-camera';
+import { TakeScreen } from '..';
+
+class Camera extends PureComponent {
+
+  state = {
     visible: false,
     imgUri: null,
     error: null,
     loading: false,
-  });
-
-  const submit = () => {
-    // mathc image
-    /* axios({
-      method: 'get',
-      url: 'https://api.pixlab.io/facecompare',
-      params: {
-        src: imgUri,
-        target: target,
-        key: 'My_Pix_Key',
-      }
-    }).then(data => {
-      console.log(data.same_face, data.confidence)
-      // submit attendance if face match
-    }).catch(error => {
-      console.log(error)
-    }) */
-    setState({ ...state, visible: true });
-  };
-  const checkSignInCode = () => {
-    const { next, saveState } = props;
-    // Save state for use in other steps
-    saveState({ name: 'samad' });
-    next();
   };
 
-  const launchCamera = () => {
-    let options = {
-      cameraRoll: false,
-      /* storageOptions: {
-        // skipBackup: true,
-        // path: 'images',
-      }, */
-    };
-    ImagePicker.launchCamera(options, (response) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else {
-        setState({ ...state, imgUri: response.uri });
-        // check for facial match
-      }
-    });
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
   };
-  const { imgUri, visible } = state;
-  const RenderImage = () => {
-    if (imgUri) {
-      return <Image source={{ uri: imgUri }} style={styles.image} />;
+
+  RenderImage = () => {
+    if (this.state.imgUri) {
+      return <Image source={{ uri: this.state.imgUri }} style={styles.image} />;
     } else {
       return (
         <>
           <Icon
             name="camera"
             style={styles.icon}
-            fill={theme['color-primary-default']}
-            onPress={launchCamera}
-          />
-          <Text
+            fill='#00BA4A'
+            onPress={this.takePicture.bind(this)} />
+          <MyText
             appearance="hint"
             style={{ ...styles.subtitleText, textAlign: 'center' }}>
             Click to take a front facing picture of yourself
-          </Text>
+          </MyText>
         </>
       );
     }
   };
-  return (
-    <>
-      <Container customStyle={styles.welcomeNote}>
-        <Text style={styles.boldText}>
-          Super! <Text style={styles.normalText}>Just a Picture</Text>
-        </Text>
-        <Text appearance="hint" style={styles.subtitleText}>
-          All that's left is just a picture, provide a selfie image of yourself
-          and you're good to go
-        </Text>
-      </Container>
-      <Container customStyle={styles.imageContainer}>
-        <View style={styles.camera}>
-          <RenderImage />
-        </View>
-        <Text appearance="hint" style={styles.subtitleText}>
-          {imgUri
-            ? 'Good! Now we need to comfirm if it is really you'
-            : 'Waiting for you to take picture'}
-        </Text>
-        {imgUri ? (
-          <Button onPress={launchCamera} style={styles.btn} status="success">
-            Retake Image
-          </Button>
-        ) : null}
-        <Button onPress={submit} style={styles.btn}>
-          Done
-        </Button>
-      </Container>
-      <ModalAlert
-        isVisible={visible}
-        closeModal={() => setState({ ...state, visible: false })}
-        message="You've signed in for Compiler Construction successfully"
-        subtitle="Ensure that you signout when the class is over, so your attendance is marked"
-        btnText="Go Home"
-      />
-    </>
-  );
-};
 
-// export default inject('store')(observer(StepTwo));
-export default Camera;
-const themedStyles = StyleService.create({
+  render() {
+    const { imgUri, visible } = this.state
+    return (
+      <TakeScreen>
+        <Container customStyle={styles.welcomeNote}>
+          <MyText customStyle={styles.boldText}>
+            Super! <MyText customStyle={styles.normalText}> Just a Picture </MyText>
+          </MyText>
+          <MyText customStyle={styles.subtitleText}>
+            All that 's left is just a picture, provide a selfie image of yourself
+            and you 're good to go
+        </MyText>
+        </Container>
+        <Container customStyle={styles.imageContainer}>
+          <View style={styles.camera}>
+            <this.RenderImage />
+            <RNCamera
+              ref={ref => {
+                this.camera = ref;
+              }}
+              captureAudio={false}
+              style={styles.preview}
+              type={RNCamera.Constants.Type.front}
+              flashMode={RNCamera.Constants.FlashMode.on}
+              androidCameraPermissionOptions={{
+                title: 'Permission to use camera',
+                message: 'We need your permission to use your camera',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+              }}
+            />
+          </View>
+          <Text appearance="hint" style={styles.subtitleText}>
+            {imgUri
+              ? 'Good! Now we need to comfirm if it is really you'
+              : 'Waiting for you to take picture'}
+          </Text>
+          {imgUri ? (
+            <Button onPress={this.takePicture.bind(this)} style={styles.btn} status="success">
+              Retake Image
+            </Button>
+          ) : null}
+          <Button onPress={() => { }} style={styles.btn}>
+            Done
+        </Button>
+        </Container>
+        <ModalAlert
+          isVisible={visible}
+          closeModal={() => setState({ ...state, visible: false })}
+          message="You've signed in for Compiler Construction successfully"
+          subtitle="Ensure that you signout when the class is over, so your attendance is marked"
+          btnText="Go Home"
+        />
+      </TakeScreen>
+    );
+  }
+
+
+}
+
+export default Camera
+const styles = StyleSheet.create({
   welcomeNote: {
-    marginTop: hp('10%'),
+    marginTop: hp('5%'),
     paddingLeft: '9%',
     paddingRight: '9%',
     height: hp('12%'),
@@ -156,7 +138,7 @@ const themedStyles = StyleService.create({
   imageContainer: {
     display: 'flex',
     /* borderColor: 'yellow',
-    borderWidth: 1, */
+            borderWidth: 1, */
     paddingLeft: '9%',
     paddingRight: '9%',
     marginTop: hp('2%'),
@@ -165,7 +147,7 @@ const themedStyles = StyleService.create({
   },
   camera: {
     display: 'flex',
-    borderColor: 'color-primary-200',
+    borderColor: '#94F89E',
     borderWidth: 1,
     borderRadius: 10,
     width: wp('80%'),

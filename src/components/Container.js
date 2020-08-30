bcc75d3dbc1630e5ec1639f429895b387bd808b8
Oluwamuyiwa David <fashmuyidavid@gmail.com>
2020-08-30 17:44:49 +0100

    added theme style

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Layout} from '@ui-kitten/components';
+import { Layout, useStyleSheet, StyleService } from '@ui-kitten/components';
 
-const Container = ({children, customStyle}) => {
-  const style = {...styles.container, ...customStyle};
+const Container = ({ children, customStyle }) => {
+  const styles = useStyleSheet(themedStyles);
+  const style = { ...styles.container, ...customStyle };
 
   return <Layout style={style}>{children}</Layout>;
 };
 
 export default Container;
 
-const styles = StyleSheet.create({
+const themedStyles = StyleService.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     paddingLeft: '5%',
     paddingRight: '5%',
+    backgroundColor: 'background-basic-color-1',
   },
 });
--- src/components/ScrollContainer.js
+++ src/components/ScrollContainer.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
-const ScrollContainer = ({children, customStyle}) => {
-  const style = {...styles.container, ...customStyle};
+import { ScrollView } from 'react-native';
+import { useStyleSheet, StyleService } from '@ui-kitten/components';
+
+const ScrollContainer = ({ children, customStyle }) => {
+  const styles = useStyleSheet(themedStyles);
+  const style = { ...styles.container, ...customStyle };
 
   return <ScrollView style={style}>{children}</ScrollView>;
 };
 
 export default ScrollContainer;
 
-const styles = StyleSheet.create({
+const themedStyles = StyleService.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     paddingLeft: '5%',
     paddingRight: '5%',
+    backgroundColor: 'background-basic-color-1',
+
   },
 });

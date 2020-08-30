eaf6d0409878c3e7904eae2e338090a015797725
Oluwamuyiwa David <fashmuyidavid@gmail.com>
2020-08-30 17:45:05 +0100

    initial commit

---
+++ src/components/MyText.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { useStyleSheet, StyleService } from '@ui-kitten/components';
+
+const MyText = ({ children, customStyle }) => {
+    const styles = useStyleSheet(themedStyles);
+    const style = { ...customStyle, ...styles.my_text }
+    return (
+        <>
+            <Text style={style}>{children}</Text>
+        </>
+    )
+}
+
+export default MyText
+const themedStyles = StyleService.create({
+    my_text: {
+        color: 'color-text'
+    }
+})

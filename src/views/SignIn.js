a055d5a46193b319b74e97dd0f4710ada6cfecff
Oluwamuyiwa David <fashmuyidavid@gmail.com>
2020-08-30 15:29:58 +0100

    add loggedin state

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Hero, FormBody, ScrollContainer} from '../components';
-import {Icon, Input, Button} from '@ui-kitten/components';
+import { StyleSheet } from 'react-native';
+import { Hero, FormBody, ScrollContainer } from '../components';
+import { Icon, Input, Button } from '@ui-kitten/components';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import axios from 'axios';
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 
 const SignInScreen = (props) => {
   const [matric_no, setMatricNo] = React.useState('F/HD/18/3210023');
@@ -27,22 +27,26 @@ const SignInScreen = (props) => {
   const handleLogin = () => {
     console.log('clicked');
     setLoading(true);
-    axios({
-      method: 'post',
-      url: `http://192.168.43.102:3333/students/login`,
-      timeout: 20000,
-      data: {
-        matric_no: matric_no,
-      },
-    })
-      .then((data) => {
-        setLoading(false);
-        console.log(data);
-      })
-      .catch((error) => {
-        setLoading(false);
-        console.log(error);
-      });
+    const { setIsLoggedIn } = props.store
+    setIsLoggedIn(true)
+    setLoading(false);
+
+    /*  axios({
+                 method: 'post',
+                 url: `http://192.168.43.102:3333/students/login`,
+                 timeout: 20000,
+                 data: {
+                     matric_no: matric_no,
+                 },
+             })
+             .then((data) => {
+                 setLoading(false);
+                 console.log(data);
+             })
+             .catch((error) => {
+                 setLoading(false);
+                 console.log(error);
+             }); */
   };
 
   return (
@@ -68,7 +72,6 @@ const SignInScreen = (props) => {
         <Button onPress={handleLogin}>
           {loading ? 'Signing in ...' : ' Sign In'}
         </Button>
-
         <Button
           onPress={() => props.navigation.navigate('home')}
           appearance="ghost">

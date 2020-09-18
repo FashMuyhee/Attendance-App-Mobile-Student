45fac5dee66c8fcd16585c86c82a082cce033193
Oluwamuyiwa David <fashmuyidavid@gmail.com>
2020-09-18 10:17:11 +0100

    change close icon

--- src/components/CameraModal.js
+++ src/components/CameraModal.js
@@ -14,17 +14,9 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-const ModalAlert = ({
-  isVisible,
-  closeModal,
- children
-}) => {
+const ModalAlert = ({isVisible, closeModal, children}) => {
   const styles = useStyleSheet(themedStyles);
   const theme = useTheme();
-
-const CloseBtn= (style) => (
-    <Icon {...style} fill="red" name="settings-outline"  onPress={closeModal}/>
-  );
 
   return (
     <Modal
@@ -35,12 +27,12 @@ const CloseBtn= (style) => (
       <Layout style={styles.content}>
         {children}
 
-<Icon
-              name="close"
-              style={styles.icon}
-              fill={theme['color-danger-300']}
-              onPress={closeModal}
-            />
+        <Icon
+          name="arrow-back"
+          style={styles.icon}
+          fill="white"
+          onPress={closeModal}
+        />
       </Layout>
     </Modal>
   );
@@ -51,48 +43,11 @@ const themedStyles = StyleService.create({
   content: {
     width: wp('100%'),
     height: hp('100%'),
-    paddingLeft: wp('10%'),
-    paddingRight: wp('10%'),
-    paddingTop: hp('20%'),
-    display: 'flex',
-    alignItems: 'center',
-  },
-  iconContainer: {
-    backgroundColor: 'color-primary-default',
-    height: hp('20%'),
-    width: wp('35%'),
-    borderRadius: 100,
-  },
-  iconWarnContainer: {
-    // backgroundColor: 'color-danger-default',
-    height: hp('20%'),
-    width: wp('30%'),
-    borderRadius: 100,
+    paddingLeft: wp('2%'),
+    paddingTop: hp('2%'),
   },
   icon: {
-    width: wp('25%'),
-    height: hp('20%'),
-    alignSelf: 'center',
-  },
-  textContainer: {
-    textAlign: 'center',
-    width: wp('50%'),
-  },
-  boldText: {
-    fontWeight: 'bold',
-    textAlign: 'center',
-    fontSize: hp('2.3%'),
-    paddingTop: hp('10%'),
-    paddingBottom: hp('5%'),
-  },
-  subtitleText: {
-    textAlign: 'center',
-    fontSize: hp('2%'),
-    paddingBottom: hp('5%'),
-  },
-  btn: {
-    width: '100%',
-    marginTop: hp('4%'),
-    marginBottom: hp('4%'),
+    width: wp('10%'),
+    height: hp('5%'),
   },
 });
--- src/views/step/Camera.js
+++ src/views/step/Camera.js
@@ -1,64 +1,65 @@
-import React, { PureComponent } from 'react';
-import { StyleSheet, View,Image } from 'react-native';
-import {
-  StyleService,
-  useStyleSheet,
-  Text,
-  Button,
-  Icon,
-  useTheme,
-} from '@ui-kitten/components';
-import { Container, ModalAlert,CameraModal, MyText } from '../../components';
+import React, {PureComponent} from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+import {Text, Button, Icon} from '@ui-kitten/components';
+import {Container, ModalAlert, CameraModal, MyText} from '../../components';
 import ImagePicker from 'react-native-image-picker';
-import { inject, observer } from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 // import axios from 'axios';
-import { RNCamera } from 'react-native-camera';
-import { TakeScreen } from '..';
+import {RNCamera} from 'react-native-camera';
+import {TakeScreen} from '..';
 
 class Camera extends PureComponent {
-
   state = {
     visible: false,
     imgUri: null,
     error: null,
     loading: false,
-cameraVisible:false
+    cameraVisible: false,
   };
 
   takePicture = async () => {
     if (this.camera) {
-
-      const options = { quality: 0.5, base64: true };
-		await this.camera.resumePreview();
+      const options = {quality: 1, base64: true,};
+      await this.camera.resumePreview();
       const data = await this.camera.takePictureAsync(options);
       console.log(data.uri);
-//send captured image for comparison 
-this.setState({imgUri:data.uri })
+      //send captured image for comparison
+      this.setState({imgUri: data.uri});
     }
   };
-onFaceDetect = async (faces) => {
-    //await this.takePicture()
-	//this.setState({cameraVisible:false})
+  onFaceDetect = async (faces) => {
+    await this.takePicture();
+    this.setState({cameraVisible: false});
+  };
+
+  handleCameraModal = () => {
+    const {cameraVisible} = this.state;
+    if (cameraVisible) {
+      this.setState({cameraVisible: false});
+    } else {
+      this.setState({cameraVisible: true});
+    }
   };
 
   RenderImage = () => {
     if (this.state.imgUri) {
-      return <Image source={{ uri: this.state.imgUri }} style={styles.image} />;
+      return <Image source={{uri: this.state.imgUri}} style={styles.image} />;
     } else {
       return (
         <>
           <Icon
             name="camera"
             style={styles.icon}
-            fill='#00BA4A'
-            onPress={()=>this.setState({cameraVisible:true})} />
+            fill="#00BA4A"
+            onPress={this.handleCameraModal}
+          />
           <MyText
             appearance="hint"
-            style={{ ...styles.subtitleText, textAlign: 'center' }}>
+            style={{...styles.subtitleText, textAlign: 'center'}}>
             Click to take a front facing picture of yourself
           </MyText>
         </>
@@ -67,24 +68,22 @@ onFaceDetect = async (faces) => {
   };
 
   render() {
-    const { imgUri, visible,cameraVisible } = this.state
+    const {imgUri, visible, cameraVisible} = this.state;
     return (
       <TakeScreen>
         <Container customStyle={styles.welcomeNote}>
           <MyText customStyle={styles.boldText}>
-            Super! <MyText customStyle={styles.normalText}> Just a Picture </MyText>
+            Super!{' '}
+            <MyText customStyle={styles.normalText}> Just a Picture </MyText>
           </MyText>
           <MyText customStyle={styles.subtitleText}>
-            All that 's left is just a picture, provide a selfie image of yourself
-            and you 're good to go
-        </MyText>
+            All that 's left is just a picture, provide a selfie image of
+            yourself and you 're good to go
+          </MyText>
         </Container>
         <Container customStyle={styles.imageContainer}>
           <View style={styles.camera}>
-            
-            
-<this.RenderImage />
-
+            <this.RenderImage />
           </View>
           <Text appearance="hint" style={styles.subtitleText}>
             {imgUri
@@ -92,52 +91,56 @@ onFaceDetect = async (faces) => {
               : 'Waiting for you to take picture'}
           </Text>
           {imgUri ? (
-            <Button onPress={()=>this.setState({cameraVisible:true})} style={styles.btn} status="success">
+            <Button
+              onPress={this.handleCameraModal}
+              style={styles.btn}
+              status="success">
               Retake Image
             </Button>
           ) : null}
-          <Button onPress={() => { }} style={styles.btn}>
+          <Button onPress={() => {}} style={styles.btn}>
             Done
-        </Button>
+          </Button>
         </Container>
         <ModalAlert
           isVisible={visible}
-          closeModal={() => setState({ ...state, visible: false })}
+          closeModal={() => setState({...state, visible: false})}
           message="You've signed in for Compiler Construction successfully"
           subtitle="Ensure that you signout when the class is over, so your attendance is marked"
           btnText="Go Home"
         />
-<CameraModal
+        <CameraModal
           isVisible={cameraVisible}
-closeModal={()=>this.setState({cameraVisible:false})}
-          >
-<RNCamera
-              ref={ref => {
-                this.camera = ref;
-              }}
-              captureAudio={false}
-              style={StyleSheet.absoluteFill}
-              type={RNCamera.Constants.Type.front}
-              flashMode={RNCamera.Constants.FlashMode.on}
-              androidCameraPermissionOptions={{
-                title: 'Permission to use camera',
-                message: 'We need your permission to use your camera',
-                buttonPositive: 'Ok',
-                buttonNegative: 'Cancel',
-              }}
-faceDetectionClassification={RNCamera.Constants.FaceDetection.Classifications.all}
-faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
-onFacesDetected={this.onFaceDetect}
-            />
+          closeModal={this.handleCameraModal}>
+          <RNCamera
+            ref={(ref) => {
+              this.camera = ref;
+            }}
+            captureAudio={false}
+            style={StyleSheet.absoluteFill}
+            type={RNCamera.Constants.Type.front}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'We need your permission to use your camera',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            faceDetectionClassification={
+              RNCamera.Constants.FaceDetection.Classifications.all
+            }
+            faceDetectionLandmarks={
+              RNCamera.Constants.FaceDetection.Landmarks.all
+            }
+            onFacesDetected={this.onFaceDetect}
+          />
         </CameraModal>
       </TakeScreen>
     );
   }
-
-
 }
 
-export default Camera
+export default Camera;
 const styles = StyleSheet.create({
   welcomeNote: {
     marginTop: hp('5%'),

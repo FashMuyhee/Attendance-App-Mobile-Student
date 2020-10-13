e66dd12eed2cc04dcb0658dc02306e0691a7cd5f
unknown <fashmuyidavid@gmail.com>
2020-10-13 19:55:59 +0100

    added fontFamily

--- src/components/MyText.js
+++ src/components/MyText.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { useStyleSheet, StyleService } from '@ui-kitten/components';
+import React from 'react';
+import {Text} from 'react-native';
+import {useStyleSheet, StyleService} from '@ui-kitten/components';
 
-const MyText = ({ children, customStyle }) => {
-    const styles = useStyleSheet(themedStyles);
-    const style = { ...customStyle, ...styles.my_text }
-    return (
-        <>
-            <Text style={style}>{children}</Text>
-        </>
-    )
-}
+const MyText = ({children, customStyle}) => {
+  const styles = useStyleSheet(themedStyles);
+  const style = {...customStyle, ...styles.my_text};
+  return (
+    <>
+      <Text style={style}>{children}</Text>
+    </>
+  );
+};
 
-export default MyText
+export default MyText;
 const themedStyles = StyleService.create({
-    my_text: {
-        color: 'color-text'
-    }
-})
+  my_text: {
+    color: 'color-text',
+    fontFamily: 'Poppins-Regular',
+  },
+});

446a6a88679bad8fb339840756b08fd5488d225c
FashMuyhee <fashmuyidavid@gmail.com>
2021-01-18 22:13:34 +0100

    added new table componet

--- src/views/MyCourse.js
+++ src/views/MyCourse.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import {
-  Divider,
   Icon,
   Text,
   TopNavigationAction,
-  styled,
-  Button,
+  StyleService,
+  useStyleSheet,
+  Layout,
 } from '@ui-kitten/components';
-import {Container, Navbar, WelcomeNote} from '../components';
+import {
+  ScrollContainer,
+  Navbar,
+  WelcomeNote,
+  Fab,
+  Table,
+  THead,
+  TBody,
+  TCell,
+  TRow,
+} from '../components';
 import {inject, observer} from 'mobx-react';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 const BackIcon = (style) => <Icon {...style} name="arrow-back" fill="white" />;
 
 const MyCourseScreen = ({navigation, store}) => {
+  const styles = useStyleSheet(themedStyles);
+  const [table, setTable] = useState({
+    head: ['S/N', 'Course Title', 'Course Code', 'Unit'],
+    data: [
+      {course: 'Course 1', id: 1, code: 'com123', unit: 3},
+      {course: 'Course 2', id: 2, code: 'com123', unit: 3},
+      {course: 'Course 3', id: 3, code: 'com123', unit: 3},
+      {course: 'Course 4', id: 4, code: 'com123', unit: 3},
+      {course: 'Course 5', id: 5, code: 'com123', unit: 3},
+      {course: 'Course 6', id: 6, code: 'com123', unit: 3},
+      {course: 'Course 7', id: 7, code: 'com123', unit: 3},
+      {course: 'Course 8', id: 8, code: 'com123', unit: 3},
+    ],
+  });
+
   const navigateBack = () => {
     navigation.goBack();
   };
@@ -29,23 +56,67 @@ const MyCourseScreen = ({navigation, store}) => {
         leftAction={<BackAction />}
         textStyle={styles.title}
       />
-      <Container>
+      <ScrollContainer customStyle={styles.container}>
         <WelcomeNote
-          bold={`Hi ${user.name.split(" ")[0]}`}
+          bold={`Hi ${user.name.split(' ')[0]}`}
           normal="Here are your courses"
           subtitle="below ate the courses offered by you,you can choose to add more at any given time by by clicking the `+` button"
         />
-        <Button onPress={() => navigation.navigate('add_course')}>
-          Add Course
-        </Button>
-      </Container>
+        <Table>
+          <THead>
+            {table.head.map((data, key) => {
+              return (
+                <TCell key={key}>
+                  <Text style={{color: 'white', textTransform: 'uppercase'}}>
+                    {data}
+                  </Text>
+                </TCell>
+              );
+            })}
+          </THead>
+          <TBody>
+            {table.data.map((item, key) => {
+              return (
+                <TRow key={key}>
+                  <TCell>
+                    <Text style={styles.tableText}>{item.id}</Text>
+                  </TCell>
+                  <TCell>
+                    <Text style={styles.tableText}>{item.course}</Text>
+                  </TCell>
+                  <TCell>
+                    <Text style={styles.tableText}>{item.code}</Text>
+                  </TCell>
+                  <TCell>
+                    <Text style={styles.tableText}>{item.unit}</Text>
+                  </TCell>
+                </TRow>
+              );
+            })}
+          </TBody>
+        </Table>
+      </ScrollContainer>
+      <Fab onPress={() => navigation.navigate('add_course')} />
     </>
   );
 };
 
 export default inject('store')(observer(MyCourseScreen));
-const styles = StyleSheet.create({
+const themedStyles = StyleService.create({
   title: {
     color: 'white',
   },
+  container: {
+    width: '100%',
+    position: 'relative',
+    height: '100%',
+  },
+  head: {
+    height: 40,
+    backgroundColor: '#808B97',
+  },
+  tableText: {
+    textTransform: 'capitalize',
+  },
+  text: {textAlign: 'center'},
 });

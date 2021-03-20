7277d5d65ad3ada81919c6e1988c385f1d07bded
FashMuyhee <fashmuyidavid@gmail.com>
2021-03-20 12:34:00 +0100

    finsh up add course for students

---
+++ src/controller/course.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import env from '../helpers/env';
+
+const fetchAllCourses = async () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${env.url}/courses`, {timeout: 30000})
+      .then(({data}) => {
+        resolve(data.payload.data.courses);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const fetchCoursesByLevel = async (level) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${env.url}/courses/fetch_by_level/${level}`, {timeout: 30000})
+      .then(({data}) => {
+        const courses = data.payload.map((course, key) => {
+          return {
+            name: course.title,
+            code: course.code,
+            id: course.id,
+          };
+        });
+        resolve(courses);
+      })
+      .catch((error) => {
+        reject(error.response);
+      });
+  });
+};
+
+const studentAddCourse = async (course_id, user) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      url: `${env.url}/students/2/add_course`,
+      method: 'post',
+      timeout: 30000,
+      headers: {Authorization: `Bearer ${user}`},
+      data: {
+        course_id,
+      },
+    })
+      .then(({data}) => {
+        resolve(data.payload);
+      })
+      .catch((error) => {
+        reject(error.response);
+      });
+  });
+};
+
+export {fetchAllCourses, fetchCoursesByLevel, studentAddCourse};
--- src/views/student/AddCourse.js
+++ src/views/student/AddCourse.js
@@ -1,31 +1,81 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {
-  Divider,
   Icon,
   Text,
   TopNavigationAction,
-  styled,
   Button,
-  Select,
+  StyleService,
+  useStyleSheet,
 } from '@ui-kitten/components';
 import {Container, Navbar, WelcomeNote} from '../../components';
 import {inject, observer} from 'mobx-react';
 import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {fetchCoursesByLevel, studentAddCourse} from '../../controller/course';
+import Snackbar from 'react-native-snackbar';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 const BackIcon = (style) => <Icon {...style} name="arrow-back" fill="white" />;
 
 const AddCourseScreen = ({navigation, store}) => {
+  const styles = useStyleSheet(themeStyle);
+
   const navigateBack = () => {
     navigation.goBack();
   };
-  const {user} = store;
-  
+  const {user, userToken} = store;
+
   const BackAction = () => (
     <TopNavigationAction icon={BackIcon} onPress={navigateBack} />
   );
-  const data = [{text: 'Option 1'}, {text: 'Option 2'}, {text: 'Option 3'}];
-  const [selectedOption, setSelectedOption] = useState(null);
+
+  const [value, setValue] = useState(null);
+  const [data, setData] = React.useState({name: 'Loading....'});
+  const [loading, setLoading] = React.useState(false);
+
+  const level =
+    user.level === 'ND1'
+      ? 1
+      : user.level === 'ND2'
+      ? 2
+      : user.level === 'HND1'
+      ? 3
+      : user.level === 'HND2'
+      ? 4
+      : null;
+
+  const onSelect = (text) => {
+    setValue({...text});
+  };
+
+  const handleAddCourse = () => {
+    setLoading(true);
+    studentAddCourse(value.id, userToken)
+      .then((data) => {
+        setLoading(false);
+        Snackbar.show({
+          text: data.message,
+          duration: Snackbar.LENGTH_SHORT,
+          textColor: 'white',
+        });
+        console.log(data);
+      })
+      .catch((e) => {
+        setLoading(false);
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    fetchCoursesByLevel(level)
+      .then((data) => {
+        setData(data);
+      })
+      .catch((e) => {
+        console.log('not fetched 😞💔😓');
+        console.log(e);
+      });
+  }, []);
 
   return (
     <>
@@ -40,27 +90,54 @@ const AddCourseScreen = ({navigation, store}) => {
           normal="Let's add some courses"
           subtitle="Select your department,level,semester and the desired courses you need to add"
         />
-        <Select
-          data={data}
-          selectedOption={selectedOption}
-          onSelect={setSelectedOption}
-          style={styles.input}
-          placeholder="Select Courses"
+        <SearchableDropdown
+          onTextChange={(text) => console.log(text)}
+          onItemSelect={onSelect}
+          selectedItems={value}
+          containerStyle={{padding: 5}}
+          textInputStyle={styles.dropdownInput}
+          itemStyle={styles.dropdownItem}
+          itemTextStyle={styles.dropdownItemText}
+          items={data}
+          defaultIndex={2}
+          placeholder="Enter Course..."
+          resetValue={false}
+          underlineColorAndroid="transparent"
         />
-        <Button>Register Course</Button>
+        <Button
+          style={{marginTop: 30}}
+          onPress={handleAddCourse}
+          disabled={loading}>
+          Register Course
+        </Button>
       </Container>
     </>
   );
 };
 
 export default inject('store')(observer(AddCourseScreen));
-const styles = StyleSheet.create({
+
+const themeStyle = StyleService.create({
   title: {
     color: 'white',
   },
-  input: {
-    marginBottom: hp('2%'),
-    padding: 20,
+  dropdownInput: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: 'color-primary-500',
+    backgroundColor: 'background-basic-color-1',
+    borderRadius: 5,
+    height: hp(5),
+    color: 'color-text',
+    placeholderColor: 'color-text',
+  },
+  dropdownItem: {
+    padding: 10,
+    backgroundColor: 'background-basic-color-1',
+    borderColor: 'color-primary-500',
+    borderWidth: 0.5,
+  },
+  dropdownItemText: {
+    color: 'color-text',
   },
 });
-``;

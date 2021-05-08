8eb4c518799065c4bbe11cf65970e3b2c21f9b04
FashMuyhee <fashmuyidavid@gmail.com>
2021-05-08 02:37:44 +0100

    added attendance query screen

--- src/views/lecturer/index.js
+++ src/views/lecturer/index.js
@@ -2,10 +2,12 @@ import AddCourseScreen from './AddCourse';
 import CreateAttendanceScreen from './CreateAttendance';
 import MyCourseScreen from './MyCourse';
 import AttendanceRecordScreen from './AttendanceRecord';
+import AttendanceQueryScreen from './AttendanceQuery';
 
 export {
   AddCourseScreen as LectAddCourse,
   CreateAttendanceScreen,
   MyCourseScreen as LectCourse,
   AttendanceRecordScreen as LectRecord,
+  AttendanceQueryScreen,
 };
